a8e6740510d08949375806a4da0fdc8af0f867c6
rafikmutawakil-sketch <rafikmutawakil@gmail.com>
2026-07-03 18:03:07 +0700

    Add files via upload

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,21 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
-use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    public function index(Request $request)
+
+    public function index()
     {
-        // Mengambil nomor meja dari query string (misal: ?meja=M01) dan simpan di session
-        if ($request->has('meja')) {
-            session(['nomor_meja' => $request->meja]);
-        }
+        $menus=Menu::all();
 
-        $kategoris = Kategori::with('menus')->get();
-        $menus = Menu::where('status_stok', 'Tersedia')->get();
-
-        return view('home', compact('kategoris', 'menus'));
+        return view('admin.menu.index',compact('menus'));
     }
+
+    public function create()
+    {
+        return view('admin.menu.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        Menu::create($request->all());
+
+        return redirect('/menu');
+
+    }
+
+    public function edit(Menu $menu)
+    {
+        return view('admin.menu.edit',compact('menu'));
+    }
+
+    public function update(Request $request,Menu $menu)
+    {
+
+        $menu->update($request->all());
+
+        return redirect('/menu');
+
+    }
+
+    public function destroy(Menu $menu)
+    {
+
+        $menu->delete();
+
+        return back();
+
+    }
+
 }
